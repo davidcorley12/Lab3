7caca3d72d661c2ad91310bbc5975dd8e32e92ee
davidcorley12 <114423568+davidcorley12@users.noreply.github.com>
2022-10-16 21:00:41 +0100

    Update App.js
    Comments added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//Import the content, header, and components files so they can be displayed from this single page react app
 import { Content } from './components/content';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
+//Import the bootstrap assets from bootstrap using react
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -17,12 +19,15 @@ import {
 import {Read} from './components/read';
 import {Create} from './components/create';
 
+//create a child class so that we can use the methods from the component class
 class App extends React.Component {
   render() {
     return (
 
       <Router>
         <div className="App">
+      
+ //Adds the bootstrap navbar
           <Navbar bg="dark" variant="dark">
             <Container>
               <Navbar.Brand href="/">Book App</Navbar.Brand>
@@ -33,7 +38,8 @@ class App extends React.Component {
               </Nav>
             </Container>
           </Navbar>
-
+      
+//Allows the functionality to display differnt pages using the navbar
           <Routes>
             <Route path='/' element={<Content></Content>} exact></Route>
             <Route path='/read' element={<Read/>} exact></Route>
